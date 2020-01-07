7f27c6cdb470fd7842acb3086a97b6c9cb91817e
samopang <pamela.gaoyun@gmail.com>
2020-01-07 12:45:33 -0500

    react RootComponent App

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+/**
+ * Root Component
+ * with render() function
+ */
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World
+        <div className="header">here is header</div>
+        <div className="body">here is body</div>
+        <div className="footer">here is footer</div>
+      </div>
+    );
+  }
 }
 
 export default App;
