5a41af53ac8c80d71c5af6caa2acc7b24ef73f7c
samopang <pamela.gaoyun@gmail.com>
2020-01-08 13:04:44 -0500

    finish left-state-up in multiple components

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Counter from './components/Counter'
 
-class App extends Component {
+export default class App extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  handleIncrementBtn = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  handleDecrementBtn = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Counter />       
+        {/* useful for communicating/synchronizing multiple components */}
+        <Counter 
+          count={this.state.count} 
+          handleIncrementBtn={this.handleIncrementBtn}
+          handleDecrementBtn={this.handleDecrementBtn}
+        />    
+        <Counter 
+          count={this.state.count} 
+          handleIncrementBtn={this.handleIncrementBtn}
+          handleDecrementBtn={this.handleDecrementBtn}
+        /> {/* here is synchronized (sharing data) */}
       </div>
     );
   }
 }
-
-export default App;
--- practice-react/src/components/Counter.js
+++ practice-react/src/components/Counter.js
@@ -1,29 +1,13 @@
 import React from 'react'
 
 export default class Counter extends React.Component {
-
-  state = {
-    count: 0
-  }
-
-  handleIncrementBtn = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  handleDecrementBtn = () => {
-    this.setState({
-      count: this.state.count - 1
-    })
-  }
-
   render() {
     return (
       <div>
         <h2>in Counter Component</h2>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handleIncrementBtn}>increment</button>
-        <button onClick={this.handleDecrementBtn}>decrement</button>
+        <div>count: {this.props.count}</div>
+        <button onClick={this.props.handleIncrementBtn}>increment</button>
+        <button onClick={this.props.handleDecrementBtn}>decrement</button>
       </div>
     )
   }
