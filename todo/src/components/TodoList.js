8d8e4467bb3efb1ac8586cbc6e82fd628fb109b5
samopang <pamela.gaoyun@gmail.com>
2020-01-08 13:48:41 -0500

    init todo react

---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+/**
+ * Todo MVC
+ * 1. add a new task
+ * 2. display all tasks
+ * 3. cross ofd task ?
+ * 4. show number of active tasks
+ * 5. filter all / active / complete
+ * 6. delete an existing task
+ * 7. delete all completed tasks
+ *  7.1 only show if at least one is completed
+ * 8. button to toggle all on/off
+ */
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        todos..
+      </div>
+    )
+  }
+}
